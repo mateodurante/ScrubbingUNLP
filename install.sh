5675ed480a68c3f3af87b178283b07818e80c816
Mateo Durante <mateodurante@gmail.com>
2022-02-06 17:44:46 -0300

    check install

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 #* [ExaBGP](https://github.com/Exa-Networks/exabgp):
-cd /opt/
-sudo git clone https://github.com/Exa-Networks/exabgp && cd exabgp/
-sudo git checkout 4.2.11
-sudo python3 -m zipapp -o /usr/local/sbin/exabgp -m exabgp.application:main  -p "/usr/bin/env python3" lib
 
+if [[ -d /opt/exabgp/ ]]; then
+    echo "Ya existe una instalación de ExaBGP en /opt/exabgp, borrando..."
+    rm -rf /opt/exabgp/
+fi
+
+echo "Descargando ExaBGP en /opt/exabgp"
+sudo git clone https://github.com/Exa-Networks/exabgp /opt/exabgp/
+cd /opt/exabgp/
+sudo git checkout 4.2.11
+
+echo "Instalando ExaBGP"
+sudo python3 -m zipapp -o /usr/local/sbin/exabgp -m exabgp.application:main  -p "/usr/bin/env python3" lib
